f5a0547895eb30f51c9a9b5c56f2291e7f9af053
dsczs <370637594@qq.com>
2017-09-27 10:09:41 +0800

    fix admin can't modify password

--- src/main/java/com/sojson/user/controller/UserCoreController.java
+++ src/main/java/com/sojson/user/controller/UserCoreController.java
@@ -77,12 +77,12 @@ public class UserCoreController extends BaseController
         pswd = UserManager.md5Pswd(email, pswd);
         UUser user = userService.login(email, pswd);
 
-        if ("admin".equals(email))
-        {
-            resultMap.put("status", 300);
-            resultMap.put("message", "管理员不准修改密码。");
-            return resultMap;
-        }
+//        if ("admin".equals(email))
+//        {
+//            resultMap.put("status", 300);
+//            resultMap.put("message", "管理员不准修改密码。");
+//            return resultMap;
+//        }
 
         if (null == user)
         {
